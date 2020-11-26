b2c61a9f3b812103daa3d81f3bc52d82bc416735
Александр Миськов <almiskov@gmail.com>
2020-11-26 23:09:37 +0300

    create: reviews section without responsive

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
-var mySwiper = new Swiper('.swiper-container', {
+var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
   parallax: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button__next',
-    prevEl: '.slider-button__prev',
+    nextEl: '.hotel-slider__button-next',
+    prevEl: '.hotel-slider__button-prev',
   },
   keyboard: {
     enabled: true,
+  }
+})
+
+var reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+  parallax: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button-next',
+    prevEl: '.reviews-slider__button-prev',
   },
+  keyboard: {
+    enabled: true,
+  }
 })
