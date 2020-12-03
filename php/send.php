d3e769dec498dfa2c0bcbce1615c34d014adaa11
Александр Миськов <almiskov@gmail.com>
2020-12-03 23:53:05 +0300

    create: send us a message form email sending

---
+++ php/send.php
@@ -0,0 +1,64 @@
+<?php
+// Файлы phpmailer
+require './phpmailer/PHPMailer.php';
+require './phpmailer/SMTP.php';
+require './phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+// Формирование самого письма
+$title = "Новое сообщение Best Tour Plan";
+$body = "
+<h2>Новое письмо</h2>
+<b>Имя:</b> $name<br>
+<b>Номер телефона:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+  $mail->isSMTP();
+  $mail->CharSet = "UTF-8";
+  $mail->SMTPAuth   = true;
+  // $mail->SMTPDebug = 2;
+  $mail->Debugoutput = function ($str, $level) {
+    $GLOBALS['status'][] = $str;
+  };
+
+  // Настройки вашей почты
+  $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+  $mail->Username   = 'glo.studying@gmail.com'; // Логин на почте
+  $mail->Password   = 'AcetilHlorid'; // Пароль на почте
+  $mail->SMTPSecure = 'ssl';
+  $mail->Port       = 465;
+  $mail->setFrom('glo.studying@gmail.com', 'Александр Миськов'); // Адрес самой почты и имя отправителя
+
+  // Получатель письма
+  $mail->addAddress('almiskov@gmail.com');
+
+  // Отправка сообщения
+  $mail->isHTML(true);
+  $mail->Subject = $title;
+  $mail->Body = $body;
+
+  // Проверяем отравленность сообщения
+  if ($mail->send()) {
+    $result = "success";
+  } else {
+    $result = "error";
+  }
+} catch (Exception $e) {
+  $result = "error";
+  $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+if ($result == 'success') {
+  echo '<h3>Сообщение отправлено!</h3>';
+} else {
+  echo '<h4>Сообщение не отправлено!</h4><br/>' . 'Причина ошибки : ' . $mail->ErrorInfo;
+}
